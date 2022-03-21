54914338db113393a2d636f987e1f734816c689d
t1_m <azi42@yahoo.com>
2022-03-21 19:48:54 +0800

    Update darkleaf.js

--- js/darkleaf.js
+++ js/darkleaf.js
@@ -1,5 +1,5 @@
 import { ethers } from './ethersv5.2.esm.min.js'
-import 'dotenv/config'
+import './dotenv/config'
 
 // A Web3Provider wraps a standard Web3 provider, which is
 // what MetaMask injects as window.ethereum into each page
